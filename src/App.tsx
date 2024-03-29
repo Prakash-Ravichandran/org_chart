c0f436ee37f6d7724b8e7e46c3e75a7a8b19b75a
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-03-29 11:56:17 +0530

    [fix]: commented allowImportingTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 
 import { Title } from "./components/Title";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="container-main flex">
