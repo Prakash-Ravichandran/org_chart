fa6c6203735e9e9110dba094da037ede535a3f10
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-03-30 17:30:46 +0530

    [feat]: integrate updateList()

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { data } from "../assets";
-import { Status } from "../interfaces";
+import { Data, Status } from "../interfaces";
 import { ContainerCards } from "./ContainerCards";
 
 const typesHero: Status[] = ["good", "normal", "bad"];
 
 export const DragAndDrop = () => {
   const [isDragging, setIsDragging] = useState(false);
+  const [listItems, setListItems] = useState<Data[]>(data);
 
   const handleDragging = (dragging: boolean) => setIsDragging(dragging);
+
+  const handleUpdateList = (id: number, status: Status) => {
+    let card = listItems.find((item) => item.id === id);
+
+    if (card && card.status !== status) {
+      card.status = status;
+
+      setListItems((prev) => [card!, ...prev.filter((item) => item.id !== id)]);
+    }
+  };
 
   return (
     <div className="grid">
@@ -16,7 +27,7 @@ export const DragAndDrop = () => {
         <ContainerCards
           status={container}
           key={container}
-          items={data}
+          items={listItems}
           isDragging={isDragging}
           handleDragging={handleDragging}
         />
