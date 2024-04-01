415949780428f5918383b8aa3a1dca5179722875
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-04-01 20:56:47 +0530

    [feat]; integrate vitest pack for test

---
+++ test/App.test.jsx
@@ -0,0 +1,16 @@
+import { describe, it } from "vitest";
+import { Search } from "../src/components/Search";
+
+describe("A truthy statement", () => {
+  it("should be equal to 2", () => {
+    // expect(1 + 1).toEqual(2);
+    <Search></Search>;
+  });
+  it("Search is Present", () => {
+    // <Search></Search>;
+    // const searchInput = getByTestId("Search an Employee...");
+    // console.log("Inside" + searchInput);
+  });
+});
+
+// Check if the input field is present
