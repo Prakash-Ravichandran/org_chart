dbb38881b81df72c50cd64709d49d778fe36a024
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-04-01 09:00:07 +0530

    [feat]: changes for netlify deployment issue

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,8 +1,14 @@
 export type Status = "Thomas Frank" | "Mikel Arteta" | "Mark Robins";
 
+// interface CustomImage {
+//   data: string;
+//   width: number;
+//   height: number;
+// }
 export interface Data {
   id: number;
   name: String;
   designation: String;
+  image: string;
   status: Status;
 }
