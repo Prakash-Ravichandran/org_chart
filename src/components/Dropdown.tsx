5e812496e109246f2d187d71bb385011e09be3a8
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-04-01 17:34:27 +0530

    [fix]: removed Duplicates options from DD

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,46 +1,20 @@
 import React from "react";
 import { DropdownProps } from "../interfaces";
 
-// interface DropdownProps {
-//   options: Array<{
-//     id: number;
-//     name: String;
-//     designation: String;
-//     status: String;
-//   }>;
-//   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-// }
-
 const Dropdown: React.FC<DropdownProps> = ({ options, onChange }) => {
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     onChange(e);
   };
 
-  //   function getUniqueListBy(
-  //     arr: Array<{
-  //       id: number;
-  //       name: String;
-  //       designation: String;
-  //       status: String;
-  //     }>,
-  //     key: Array<{
-  //       id: number;
-  //       name: String;
-  //       designation: String;
-  //       status: String;
-  //     }>
-  //   ) {
-  //     return [...new Map(arr.map((item) => [item[key], item])).values()];
-  //   }
-
-  //   console.log(getUniqueListBy(options, "designation"));
-
-  //   let result = options.map(({ designation }) => designation)
+  const ids = options.map(({ designation }) => designation);
+  const filtered = options.filter(
+    ({ designation }, index) => !ids.includes(designation, index + 1)
+  );
 
   return (
     <>
       <select onChange={handleSelectChange}>
-        {options.map((option) => (
+        {filtered.map((option) => (
           <option key={option.id} value={option.id}>
             {option.designation}
           </option>
