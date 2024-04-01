21912532ab0b9af65fcf8bca2248e7bbc3a640b3
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-04-01 08:15:45 +0530

    [feat]: integrate images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,7 +6,6 @@ export const data: Data[] = [
     name: "Linda Mary",
     designation: "Accountant",
     image: "https://bumbeishvili.github.io/avatars/avatars/portrait3.png",
-
     status: "Thomas Frank",
   },
   {
@@ -28,6 +27,20 @@ export const data: Data[] = [
     name: "Smith Mark",
     designation: "Programmer",
     image: "https://bumbeishvili.github.io/avatars/avatars/portrait28.png",
+    status: "Mikel Arteta",
+  },
+  {
+    id: 5,
+    name: "Sam Mathew",
+    designation: "Chief Accountant",
+    image: "https://bumbeishvili.github.io/avatars/avatars/portrait31.png",
+    status: "Mark Robins",
+  },
+  {
+    id: 6,
+    name: "Shanta vollmans",
+    designation: "Chief Accountant",
+    image: "https://bumbeishvili.github.io/avatars/avatars/portrait4.png",
     status: "Mikel Arteta",
   },
 ];
