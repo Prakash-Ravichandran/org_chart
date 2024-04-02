b64a515537f4ab22910581cd0aad7d715a01f2c1
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-04-02 09:19:24 +0530

    [feat]: add test for dropdown

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,8 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import Dropdown from "../src/components/Dropdown";
 import { Search } from "../src/components/Search";
 
-describe("Testing a Search Component", () => {
+const options = [
+  { designation: "Accountant" },
+  { designation: "Programmer" },
+  { designation: "Finance Officer" },
+  { designation: "Chief Accountant" },
+  { designation: "Client Representative" },
+];
+
+describe("Testing Organizational Chart", () => {
   it("Search Component is rendering", () => {
     <Search></Search>;
   });
@@ -11,6 +20,13 @@ describe("Testing a Search Component", () => {
     const searchInput = queryByPlaceholderText("Search an Employee...");
     expect(searchInput.value).toBe("");
   });
+  it("Dropdown Component is rendering", () => {
+    <Dropdown></Dropdown>;
+  });
+  it("Option is present", () => {
+    render(<Dropdown options={options} defaultValue={"Accountant"} />);
+    expect(screen.getByRole("option", { name: "Accountant" }).selected).toBe(
+      true
+    );
+  });
 });
-
-// Check if the input field is present
